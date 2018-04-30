9a7a73878001388f9562f9d4553c4fab92f59e7d
junaidNadeem <junaid.pk48@gmail.com>
2018-04-30 19:38:20 +0500

    Sign in page removed

--- js/animate.js
+++ js/animate.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
-	$('#main-content').fadeIn("slow");
-
-
+	$('.main-content').fadeIn("slow");
 	$('.laptop-container').animate({"top": '0px'}, 900);
+	$('#sign-in').click(function(){
+		$('.sign-up-main-content').fadeOut("fast");
+		$('.sign-in-main-content').fadeIn("slow");
+	});
+	$('#sign-up').click(function(){
+		$('.sign-in-main-content').fadeOut("fast");
+		$('.sign-up-main-content').fadeIn("slow");
+	});
 });
